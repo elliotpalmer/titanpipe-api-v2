2fb1bc85f17bb97bb612acb87d8f6f3d6afdca39
Elliot Palmer <palm1221ks@gmail.com>
2022-11-25 22:14:04 -0500

    update port logic

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from api import app
+import os
 
 if __name__ == "__main__":
     from api.db import db
     db.init_app(app)
-    app.run(host="0.0.0.0", port=8080, debug=True)
+    app.run(host="0.0.0.0", port=os.getenv("PORT", default=5000), debug=True)
